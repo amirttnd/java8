badffb842748ecc1fddc9d767a1470e3ab3f4345
Mohd Amir <mohd.amir@tothenew.com>
2017-10-19 17:57:35 +0530

    Scheduled thread pool

--- src/com/demo/executor/ExecutorDemo.java
+++ src/com/demo/executor/ExecutorDemo.java
@@ -1,17 +1,16 @@
 package com.demo.executor;
 
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
-/***
+/**
  * http://tutorials.jenkov.com/java-util-concurrent/executorservice.html
+ * http://tutorials.jenkov.com/java-util-concurrent/scheduledexecutorservice.html
  */
 public class ExecutorDemo {
 
     public static void main(String[] args) throws Exception {
-        singleThreadPoolFuture();
+        singleThreadPool();
     }
 
     public static void singleThreadPool() {
@@ -20,6 +19,18 @@ public class ExecutorDemo {
             System.out.println(Thread.currentThread().getName());
         });
         executorService.shutdown();
+    }
+
+    public static void scheduledThreadPool() throws Exception{
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        executorService.scheduleWithFixedDelay(() -> {
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName());
+        }, 1, 1, TimeUnit.SECONDS);
     }
 
     public static void singleThreadPoolFuture() throws Exception {
