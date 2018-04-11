389bbf6c9b43861675135f5987898f1ce2973259
Mohd Amir <mohd.amir@tothenew.com>
2018-04-11 19:57:08 +0530

    generic stack

--- src/com/demo/datastructure/Stack.java
+++ src/com/demo/datastructure/Stack.java
@@ -3,23 +3,23 @@ package com.demo.datastructure;
 
 import java.util.EmptyStackException;
 
-public class Stack {
-    int a[];
+public class Stack<T> {
+    Object []a;
     int capacity;
     int length;
 
     Stack() {
         capacity = 10;
         length = -1;
-        a = new int[capacity];
+        a = new Object[capacity];
     }
 
     Stack(int capacity) {
         this.capacity = capacity;
-        a = new int[capacity];
+        a = new Object[capacity];
     }
 
-    boolean push(int element) {
+    boolean push(T element) {
         if (length == capacity - 1) {
             System.out.println("Stack overflow");
             return false;
@@ -29,18 +29,18 @@ public class Stack {
     }
 
 
-    int pop() {
+    Object pop() {
         if (length == -1) {
             throw new EmptyStackException();
         }
         return a[length--];
     }
 
-    int peek() {
+    T peek() {
         if (length == -1) {
             throw new EmptyStackException();
         }
-        return a[length];
+        return (T)a[length];
     }
 
     int size() {
@@ -54,18 +54,20 @@ public class Stack {
     }
 
     public static void main(String[] args) {
-        Stack stack = new Stack();
+        Stack<Integer> stack = new Stack<Integer>();
         stack.push(1);
         stack.push(2);
         stack.push(3);
         stack.push(4);
         stack.push(5);
         stack.display();
-        System.out.println(stack.size());
+        stack.size();
 
-        System.out.println(stack.pop());
-        stack.display();
-        System.out.println(stack.size());
+        Stack<String> stack1=new Stack<>();
+        stack1.push("a");
+        stack1.push("b");
+        stack1.push("c");
+        stack1.display();
 
     }
 }
